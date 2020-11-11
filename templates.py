294121f87a99768faf526eb99f4c9ce869a275a5
Nic Schumann <nic@nicschumann.co>
2020-11-11 17:28:34 -0500

    [templates] adds basic error handling to templates

--- templates.py
+++ templates.py
@@ -1,5 +1,6 @@
 import os
 import json
+from GlyphsApp import *
 
 TEMPLATE_DIRECTORY = 'data'
 
@@ -15,13 +16,12 @@ class OCCTemplatesView():
         templates_files = filter(lambda f: '.json' in f, os.listdir(directory))
         templates = []
 
-        for template_name in templates_files:
+        for i, template_name in enumerate(templates_files):
             with open(os.path.join(directory, template_name), 'r') as template_file:
                 try:
                     template = json.load(template_file)
-                    row, valid = self.validateAndFormatTemplate(template)
-                    if valid:
-                        templates.append(row)
+                    row = self.validateAndFormatTemplate(template_name, template, i)
+                    templates.append(row)
 
                 except Exception as error:
                     print(error)
@@ -29,8 +29,68 @@ class OCCTemplatesView():
         return templates
 
 
-    def validateAndFormatTemplate(self, template):
-        return template, True
+    def validateAndFormatTemplate(self, template_name, template, index):
+
+        if template.has_key('name'):
+            name = template['name']
+        else:
+            print('[%s]\t"%s" does not have a template name specified. Naming it "Template %i"' % (template_name, template_name, index))
+            name = 'Template %i' % index
+
+        if template.has_key('lines'):
+            lines = []
+            for linenum, line in enumerate(template['lines']):
+                if not line.has_key('style'):
+                    print('[%s]\tline %i has no style specified, skipping...' % (template_name, linenum))
+                    continue
+
+                if not line.has_key('size'):
+                    print('[%s]\tline %i has no size specified, skipping...' % (template_name, linenum))
+                    continue
+
+                if len(filter(lambda m: m.name == line['style'], Glyphs.font.masters)) != 1:
+                    print('[%s]\tline %i does not specify an existing master in this typeface, skipping...' % (template_name, linenum))
+                    continue
+
+                if not isinstance(line['size'], int):
+                    print('[%s]\tline %i does not specify a numerical size, skipping...' % (template_name, linenum))
+                    continue
+
+                lines.append(line)
+
+        else:
+            print('[%s]\t"%s" does not have any lines specified. "' % (template_name, template_name))
+            lines = []
+
+        proof = {
+            "margins": {
+                "left": 20,
+                "right": 70,
+                "top": 20,
+                "bottom": 100,
+            },
+            "padding": {
+                "line": 20,
+                "block": 20
+            }
+        }
+
+        if template.has_key('proof'):
+            if template['proof'].has_key('margins'):
+                proof['margins']['left'] = template['proof']['margins']['left'] if hasattr(template['proof']['margins'], 'left') else 20
+                proof['margins']['right'] = template['proof']['margins']['right'] if hasattr(template['proof']['margins'], 'right') else 70
+                proof['margins']['top'] = template['proof']['margins']['top'] if hasattr(template['proof']['margins'], 'top') else 20
+                proof['margins']['bottom'] = template['proof']['margins']['bottom'] if hasattr(template['proof']['margins'], 'bottom') else 100
+
+            if template['proof'].has_key('padding'):
+                proof['padding']['line'] = template['proof']['padding']['line'] if hasattr(template['proof']['padding'], 'line') else 20
+                proof['padding']['block'] = template['proof']['padding']['block'] if hasattr(template['proof']['padding'], 'block') else 20
+
+        else:
+            print('[%s]\t"%s" does not specify margin and padding information. Setting defaults. "' % (template_name, template_name))
+
+
+        return template
 
 
     def getTemplateList(self):
