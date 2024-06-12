31e4d2068e8cab7edc646adbe8d14cbbd6af7501
Marie Otsuka <motsuka.gd@gmail.com>
2024-06-12 16:58:04 -0400

    convert to installable Glyphs3 Plugin structure
    - installed in the File menu

---
+++ ProofingTool.glyphsPlugin/Contents/Resources/plugin.py
@@ -0,0 +1,53 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Proofing Tool Plugin
+#
+#	Read the docs:
+#	https://github.com/morisawausa/ProofingTool
+#
+#
+###########################################################################################################
+
+from __future__ import division, print_function, unicode_literals
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+from proofing import OCCProofingTool
+
+class ProofingTool(GeneralPlugin):
+
+	@objc.python_method
+	def settings(self):
+		self.name = 'Proofing Tool'
+		self.proofer = None
+
+	@objc.python_method
+	def start(self):
+		newMenuItem = NSMenuItem(self.name, self.showWindow_)
+		Glyphs.menu[FILE_MENU].append(newMenuItem)
+
+	def showWindow_(self, sender):
+		"""Do something like show a window """
+		print("show Windows")
+		Glyphs.showMacroWindow()
+		if Glyphs.font:
+			print("\n❇️ Welcome to the Proofing Tool \n")
+			if Glyphs.font.instances:
+				if not self.proofer or not self.proofer.mainWindow._window:
+					self.proofer = OCCProofingTool()
+				else:
+					self.proofer.mainWindow.show()
+			else:
+				print("There are no exports set up yet 😥 Please define at least one instance and run the tool again.")
+		else:
+			print("There are no fonts open yet 😥 Please open a file and run the tool again.")
+
+
+	@objc.python_method
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
--- ProofingTool.glyphsPlugin/Contents/Resources/preferences.py
+++ ProofingTool.glyphsPlugin/Contents/Resources/preferences.py
@@ -8,12 +8,12 @@ class OCCTemplatePreferences( ):
 		self.loadPreferences()
 
 	def loadPreferences( self ):
-		Glyphs.registerDefault("com.OCC.ProofingTool.templatefiles", ['data/demo.json'])
+		Glyphs.registerDefault("com.OCC.ProofingTool.templatefiles", ['demo.json'])
 		try:
 			self.templatePaths = Glyphs.defaults["com.OCC.ProofingTool.templatefiles"]
 			if len(self.templatePaths) < 1:
 				print( 'Loading demo template, which may not match your instances. Please review and edit the template for your font.' )
-				self.templatePaths = ['data/demo.json']
+				self.templatePaths = ['demo.json']
 		except:
 			return False
 		return True
