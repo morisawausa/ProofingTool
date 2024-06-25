bcb52cafeec28bee74a1606dd93a2775bd707b32
Marie Otsuka <motsuka.gd@gmail.com>
2024-06-25 15:14:59 -0400

    Fix crash caused by `pyobjc 10.3` update
    Reference: https://forum.glyphsapp.com/t/plugin-menu-items-showing-as-nsmenuitem-in-the-app-ui-and-not-working/30289

--- ProofingTool.glyphsPlugin/Contents/Resources/plugin.py
+++ ProofingTool.glyphsPlugin/Contents/Resources/plugin.py
@@ -1,13 +1,10 @@
-# encoding: utf-8
-
+# encoding: utf-8\
 ###########################################################################################################
-#
 #
 #	Proofing Tool Plugin
 #
 #	Read the docs:
 #	https://github.com/morisawausa/ProofingTool
-#
 #
 ###########################################################################################################
 
@@ -23,28 +20,34 @@ class ProofingTool(GeneralPlugin):
 	@objc.python_method
 	def settings(self):
 		self.name = 'Proofing Tool'
+		self.menuItem = NSMenuItem.alloc().init()
+		self.menuItem.setTitle_(self.name)
+		self.menuItem.setAction_(self.showWindow_)
+		self.menuItem.setTarget_(self)
+
 		self.proofer = None
 
 	@objc.python_method
 	def start(self):
-		newMenuItem = NSMenuItem(self.name, self.showWindow_)
-		Glyphs.menu[FILE_MENU].append(newMenuItem)
+		Glyphs.menu[FILE_MENU].append(self.menuItem)
 
 	def showWindow_(self, sender):
 		"""Do something like show a window """
-		Glyphs.showMacroWindow()
-		if Glyphs.font:
-			print("\n❇️ Welcome to the Proofing Tool \n")
-			if Glyphs.font.instances:
-				if not self.proofer or not self.proofer.mainWindow._window:
-					self.proofer = OCCProofingTool()
+		try:
+			Glyphs.showMacroWindow()						
+			if Glyphs.font:
+				print("\n❇️ Welcome to the Proofing Tool \n")
+				if Glyphs.font.instances:
+					if not self.proofer or not self.proofer.mainWindow._window:
+						self.proofer = OCCProofingTool()
+					else:
+						self.proofer.mainWindow.show()
 				else:
-					self.proofer.mainWindow.show()
+					print("There are no exports set up yet 😥 Please define at least one instance and run the tool again.")
 			else:
-				print("There are no exports set up yet 😥 Please define at least one instance and run the tool again.")
-		else:
-			print("There isn’t a font open yet 😥 Please open a file and run the tool again.")
-
+				print("There isn’t a font open yet 😥 Please open a file and run the tool again.")
+		except:
+			NSLog(traceback.format_exc())
 
 	@objc.python_method
 	def __file__(self):
